4b330d91bb57c783c57816e45cd1210c7a3872ea
Pablo Mendez <mendez.developer@gmail.com>
2021-05-27 21:41:27 -0300

    added ping status

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -4,10 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 
 	flattener "github.com/mendezdev/tgo_flattener/flattener/web"
+	"github.com/mendezdev/tgo_flattener/ping"
 )
 
 func routes() *gin.Engine {
 	router := gin.Default()
+
+	router.GET("/ping", ping.Ping)
 
 	flatHandler := flattener.NewFlatHandler()
 	router.POST("/flat", flatHandler.Flat)
