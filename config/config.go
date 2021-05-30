0881038f476f1aa725605bc30a993ddd94f97ab4
Pablo Mendez <mendez.developer@gmail.com>
2021-05-30 19:56:38 -0300

    removed unnecesary things

--- config/config.go
+++ config/config.go
@@ -1,16 +1,5 @@
 package config
 
-import "os"
-
 const (
 	FlatsLimit = int64(100)
-
-	Production = "production"
-	Develop    = "develop"
-	Test       = "test"
-)
-
-var (
-	Scope       = os.Getenv("SCOPE")
-	Environment = os.Getenv("TGO_ENVIRONMENT")
 )
