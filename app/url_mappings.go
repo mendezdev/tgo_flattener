02319d6d97546db97b4e0874ab12501ece6b6d40
Pablo Mendez <mendez.developer@gmail.com>
2021-05-30 22:55:58 -0300

    improved url

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -11,7 +11,7 @@ func routes(h handlers) *gin.Engine {
 
 	router.GET("/ping", ping.Ping)
 
-	router.POST("/flat", h.Flat.Post)
+	router.POST("/flats", h.Flat.Post)
 	router.GET("/flats", h.Flat.GetAll)
 
 	return router
