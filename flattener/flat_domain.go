2e9abff4ce4e0c9f20ce8aa53eacebf18014f9af
Pablo Mendez <mendez.developer@gmail.com>
2021-05-30 23:31:56 -0300

    added comments

--- flattener/flat_domain.go
+++ flattener/flat_domain.go
@@ -9,11 +9,13 @@ import (
 	"github.com/mendezdev/tgo_flattener/apierrors"
 )
 
+// FlatResponse represents the client response for POST /flats
 type FlatResponse struct {
 	MaxDepth int           `json:"max_depth"`
 	Data     []interface{} `json:"flatted_data"`
 }
 
+// FlatResponse represents the client response for GET /flats
 type FlatInfoResponse struct {
 	ID          string        `json:"id"`
 	ProcessedAt time.Time     `json:"processed_at"`
@@ -21,6 +23,7 @@ type FlatInfoResponse struct {
 	Flatted     []interface{} `json:"flatted"`
 }
 
+// FlatInfo represents the structure to be saved in the db
 type FlatInfo struct {
 	ID             string           `json:"id" bson:"_id,omitempty"`
 	Graph          *Graph           `bson:"-"`
@@ -29,35 +32,42 @@ type FlatInfo struct {
 	ProcessedAt    time.Time        `bson:"processed_at"`
 }
 
-// Graph
+// Graph contains all the information about the array
+// knows how to flat and unflat an array
 type Graph struct {
 	Vertices map[int]*Vertex
 	directed bool
 }
 
+// Vertex represents the nodes in the Graph and the connection between each other
 type Vertex struct {
 	Key      int
 	Value    interface{}
 	Vertices map[int]*Vertex
 }
 
+// VertexSecuence contains all the nodes information to restore the flatted array
+// with the EdgeSecuence
 type VertexSecuence struct {
 	Key      int      `bson:"key"`
 	DataInfo DataInfo `bson:"data"`
 	Edges    []int    `json:"edges"`
 }
 
+// EdgeSecuence represents the connections between the nodes
 type EdgeSecuence struct {
 	From int `bson:"from"`
 	To   int `bson:"to"`
 }
 
+// DataInfo represents the information of the value of the array node
 type DataInfo struct {
 	DataType  string `bson:"type"`
 	DataValue string `bson:"value"`
 }
 
-// CONSTRUCTORS
+/* CONSTRUCTORS */
+
 func NewVertex(key int, value interface{}) *Vertex {
 	return &Vertex{
 		Key:      key,
@@ -73,7 +83,9 @@ func NewDirectedGraph() *Graph {
 	}
 }
 
-// METHODS
+/* METHODS */
+
+// ToArray will build the array with the information in the Graph
 func (g *Graph) ToArray() []interface{} {
 	res := make([]interface{}, 0)
 	for _, v := range g.Vertices[0].Vertices {
@@ -83,6 +95,7 @@ func (g *Graph) ToArray() []interface{} {
 	return res
 }
 
+// ToArray is called by Graph to build the array
 func (v *Vertex) ToArray() interface{} {
 	if len(v.Vertices) <= 0 {
 		return v.Value
@@ -97,6 +110,7 @@ func (v *Vertex) ToArray() interface{} {
 	return res
 }
 
+// ToFlat will return the flatted array with the Graph information
 func (g *Graph) ToFlat() []interface{} {
 	res := make([]interface{}, 0)
 	for _, v := range g.Vertices[0].Vertices {
@@ -111,6 +125,7 @@ func (g *Graph) ToFlat() []interface{} {
 	return res
 }
 
+// ToFlat is called by Graph to build the flaated array
 func (v *Vertex) ToFlat() interface{} {
 	if len(v.Vertices) <= 0 {
 		return v.Value
@@ -129,6 +144,8 @@ func (v *Vertex) ToFlat() interface{} {
 	return res
 }
 
+// GetVertexSecuence build the secuence necesary to be saved in db to be use
+// to rebuild the Graph and the array
 func (g *Graph) GetVertexSecuence() []VertexSecuence {
 	vtxSecuence := make([]VertexSecuence, 0)
 	for _, v := range g.Vertices {
@@ -137,6 +154,7 @@ func (g *Graph) GetVertexSecuence() []VertexSecuence {
 	return vtxSecuence
 }
 
+// GetVertexSecuence is called by Graph
 func (v *Vertex) GetVertexSecuence() VertexSecuence {
 	var dt, dv string
 	var err error
@@ -191,7 +209,30 @@ func (g *Graph) AddEdge(k1, k2 int) error {
 	return nil
 }
 
-// FUNCTIONS
+// toInterface rebuild the original value of in the array
+func (di DataInfo) toInterface() (interface{}, error) {
+	var convertedValue interface{}
+	var err error
+
+	switch di.DataType {
+	case "float64":
+		convertedValue, err = strconv.ParseFloat(di.DataValue, 64)
+	case "bool":
+		convertedValue, err = strconv.ParseBool(di.DataValue)
+	default:
+		convertedValue = di.DataValue
+	}
+	if err != nil {
+		return nil, fmt.Errorf("error parsing flat_data: %s", err.Error())
+	}
+	return convertedValue, nil
+}
+
+/* FUNCTIONS */
+
+// FlatArray it receive an input array an recursive will find
+// the max depth of the array and will build a Graph. This info is wrapped
+// in a FlatInfo
 func FlatArray(input []interface{}) (FlatInfo, apierrors.RestErr) {
 	g := NewDirectedGraph()
 
@@ -199,6 +240,8 @@ func FlatArray(input []interface{}) (FlatInfo, apierrors.RestErr) {
 	var maxDepth int
 	g.AddVertex(node, nil)
 
+	// this callback func  will create the nodes and added the connections
+	// to build the Graph. Also will track the max depth
 	cb := func(father int, depth int, val interface{}) (int, error) {
 		if depth > maxDepth {
 			maxDepth = depth
@@ -209,6 +252,10 @@ func FlatArray(input []interface{}) (FlatInfo, apierrors.RestErr) {
 			data = val
 		}
 
+		// every this cb is execute, it means that it is in a node value inside the array
+		// so add a vertex (node) to the Graph and the connection with father-son relation
+		// e.g: after added 1 to node, this is the father for the next iteration and the "father"
+		// is the node in the before iteration
 		node++
 		g.AddVertex(node, data)
 		if err := g.AddEdge(father, node); err != nil {
@@ -234,10 +281,16 @@ func FlatArray(input []interface{}) (FlatInfo, apierrors.RestErr) {
 func buildGraphRecursive(data []interface{}, father int, depth int, cb func(int, int, interface{}) (int, error)) error {
 	for _, v := range data {
 		var d int
+
+		// if it is an array, add one to depth
+		// call the function again to go more in depth and pass the info
+		// to the next iteration
 		parsed, ok := v.([]interface{})
 		if ok {
 			d = depth + 1
 		}
+
+		// current will be the father for the next iteration and actual father is for the current
 		current, err := cb(father, d, v)
 		if err != nil {
 			return err
@@ -249,16 +302,6 @@ func buildGraphRecursive(data []interface{}, father int, depth int, cb func(int,
 		}
 	}
 	return nil
-}
-
-func getTypeAndValueStringFromInterface(val interface{}) (dt string, dv string, err error) {
-	if val == nil {
-		err = errors.New("cannot get type and value from nil interface")
-		return
-	}
-	dt = fmt.Sprintf("%T", val)
-	dv = fmt.Sprintf("%v", val)
-	return
 }
 
 func BuildGraphFromVertexSecuence(vertexSecuence []VertexSecuence) (*Graph, apierrors.RestErr) {
@@ -285,20 +328,12 @@ func BuildGraphFromVertexSecuence(vertexSecuence []VertexSecuence) (*Graph, apie
 	return g, nil
 }
 
-func (di DataInfo) toInterface() (interface{}, error) {
-	var convertedValue interface{}
-	var err error
-
-	switch di.DataType {
-	case "float64":
-		convertedValue, err = strconv.ParseFloat(di.DataValue, 64)
-	case "bool":
-		convertedValue, err = strconv.ParseBool(di.DataValue)
-	default:
-		convertedValue = di.DataValue
+func getTypeAndValueStringFromInterface(val interface{}) (dt string, dv string, err error) {
+	if val == nil {
+		err = errors.New("cannot get type and value from nil interface")
+		return
 	}
-	if err != nil {
-		return nil, fmt.Errorf("error parsing flat_data: %s", err.Error())
-	}
-	return convertedValue, nil
+	dt = fmt.Sprintf("%T", val)
+	dv = fmt.Sprintf("%v", val)
+	return
 }
