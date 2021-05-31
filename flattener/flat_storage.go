2926d31460377100fc7d8b47a0949e18da813d47
Pablo Mendez <mendez.developer@gmail.com>
2021-05-30 23:09:26 -0300

    added gateway and storage comments

--- flattener/flat_storage.go
+++ flattener/flat_storage.go
@@ -20,6 +20,7 @@ const (
 	DbNameTest     = "flattenerdbtest"
 )
 
+// Storage will execute all de CRUD operations flat_info related
 type Storage interface {
 	create(FlatInfo) apierrors.RestErr
 	getAll() ([]FlatInfo, apierrors.RestErr)
@@ -47,27 +48,33 @@ func NewTestStorage(db *mongo.Client) Storage {
 func (s *storage) create(fi FlatInfo) apierrors.RestErr {
 	collection := s.db.Database(s.dbName).Collection(FlatCollection)
 	insertResult, err := collection.InsertOne(context.TODO(), fi)
+
 	if err != nil {
 		return apierrors.NewInternalServerError(fmt.Sprintf("database error creating flat_info: %s", err.Error()))
 	}
+
 	if insertedID, ok := insertResult.InsertedID.(primitive.ObjectID); ok {
 		fi.ID = insertedID.Hex()
 	}
+
 	return nil
 }
 
 func (s *storage) getAll() ([]FlatInfo, apierrors.RestErr) {
 	collection := s.db.Database(s.dbName).Collection(FlatCollection)
 	ctx := context.TODO()
+
 	findOptions := options.Find()
 	findOptions.SetSort(bson.D{{"processed_at", -1}}).SetLimit(config.FlatsLimit)
 	cursor, err := collection.Find(ctx, bson.D{}, findOptions)
 	if err != nil {
 		return nil, apierrors.NewInternalServerError(fmt.Sprintf("database error getting all flat_info: %s", err.Error()))
 	}
+
 	var res []FlatInfo
 	if cursorErr := cursor.All(ctx, &res); cursorErr != nil {
 		return nil, apierrors.NewInternalServerError(fmt.Sprintf("database error iterating cursor of all flat_info: %s", err.Error()))
 	}
+
 	return res, nil
 }
